810dcbd954d68eebc6cd633403f469f2fec00d48
Lisa Carlson <lisa.carlson85@gmail.com>
2015-11-13 12:57:34 -0700

    users can create a new memory

--- js/app.js
+++ js/app.js
@@ -1,9 +1,37 @@
 angular.module('MyApp', ['ngRoute'])
-  .controller('HomeController', ['$scope', '$http', function ($scope, $http) {
+  .controller('HomeController', ['$scope', '$http', '$location', function ($scope, $http, $location) {
+    $scope.memories = [];
     $http.get('http://g12-lisa-carlson-memories.cfapps.io/api/v1/memories').then(function(response){
-      $scope.memories = response.data.data;
-      console.log($scope.memories);
+      for (var x in response.data.data) {
+        $scope.memories.push(response.data.data[x]);
+      }
     });
+
+    $scope.submit = function() {
+      var memoryObj = {
+      "data": {
+        "type": "memory",
+        "attributes": {
+          "old_days": "string",
+          "these_days": "string",
+          "year": 0
+          }
+        }
+      }
+      memoryObj.data.attributes.old_days = $scope.memory1;
+      memoryObj.data.attributes.these_days = $scope.memory2;
+      memoryObj.data.attributes.year = $scope.year;
+      $scope.memories.push(memoryObj);
+      $http.post('http://g12-lisa-carlson-memories.cfapps.io/api/v1/memories', memoryObj).then(function(response){
+          console.log(response.data)
+      }, function(){
+          console.log('error');
+      });
+      $scope.memory1 = '';
+      $scope.memory2 = '';
+      $scope.year = '';
+    }
+    
   }])
   .controller('YearController', ['$scope', '$http', function ($scope, $http) {
     
